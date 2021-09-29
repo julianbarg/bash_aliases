9ca075e66d2e05ff8e33f59b4122b50b442ffe88
Julian Barg <barg.julian@gmail.com>
2021-09-29 11:40:12 -0400

    Fixed indentation.

--- pandoc.sh
+++ pandoc.sh
@@ -1,9 +1,9 @@
 # gollum to docx
 function gollum_to_docx {
-cp "$1" temp.md &&
-sed -i '/[[_TOC_]]/d' temp.md &&
-pandoc temp.md --citeproc --bibliography bibliography.bib --reference-doc ~/Templates/custom-reference-wiki.odt -o ~/Documents/main.odt &&
-rm temp.md &&
-libreoffice --headless --convert-to docx ~/Documents/main.odt --outdir ~/Documents  &&
-rm ~/Documents/main.odt
+  cp "$1" temp.md &&
+  sed -i '/[[_TOC_]]/d' temp.md &&
+  pandoc temp.md --citeproc --bibliography bibliography.bib --reference-doc ~/Templates/custom-reference-wiki.odt -o ~/Documents/main.odt &&
+  rm temp.md &&
+  libreoffice --headless --convert-to docx ~/Documents/main.odt --outdir ~/Documents  &&
+  rm ~/Documents/main.odt
 }

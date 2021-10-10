7a2770fa4e8b690cac8c0bb6ef06cb69db43a2c3
Julian Barg <barg.julian@gmail.com>
2021-10-10 16:32:55 -0400

    Added functionality to gollum_to_docx

--- pandoc.sh
+++ pandoc.sh
@@ -1,8 +1,13 @@
 # gollum to docx
 function gollum_to_docx {
   cp "$1" temp.md &&
+  # Remove Table of Contents command
   sed -i '/[[_TOC_]]/d' temp.md &&
-  pandoc temp.md --citeproc --bibliography bibliography.bib --reference-doc ~/Templates/custom-reference-wiki.odt -o ~/Documents/main.odt &&
+  # Remove hyperlinks
+  sed -i 's/\[\[//g' temp.md &&
+  sed -i -E 's/\|[^]]*\]\]//g' temp.md &&
+  # pandoc temp.md --toc --reference-doc ~/Templates/custom-reference-wiki.odt -o ~/Documents/main.odt &&
+  pandoc temp.md --toc --citeproc --bibliography bibliography.bib --reference-doc ~/Templates/custom-reference-wiki.odt -o ~/Documents/main.odt &&
   rm temp.md &&
   libreoffice --headless --convert-to docx ~/Documents/main.odt --outdir ~/Documents  &&
   rm ~/Documents/main.odt

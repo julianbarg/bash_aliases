548d1f86852679c683831445c094a1667b677941
Julian Barg <barg.julian@gmail.com>
2021-10-19 15:26:01 -0400

    Optimized gollum_to_docx for word desktop.

--- pandoc.sh
+++ pandoc.sh
@@ -1,14 +1,17 @@
 # gollum to docx
 function gollum_to_docx {
   cp "$1" temp.md &&
+  # Fix images
+  sed -i 's/\](\//\](.\//g' temp.md &&
   # Remove Table of Contents command
   sed -i '/[[_TOC_]]/d' temp.md &&
+  # Remove "Back to" links
+  sed -i -E '/\* Back to \[\[[^]]*\]\]/d' temp.md &&
   # Remove hyperlinks
   sed -i 's/\[\[//g' temp.md &&
   sed -i -E 's/\|[^]]*\]\]//g' temp.md &&
-  # pandoc temp.md --toc --reference-doc ~/Templates/custom-reference-wiki.odt -o ~/Documents/main.odt &&
-  pandoc temp.md --toc --citeproc --bibliography bibliography.bib --reference-doc ~/Templates/custom-reference-wiki.odt -o ~/Documents/main.odt &&
-  rm temp.md &&
-  libreoffice --headless --convert-to docx ~/Documents/main.odt --outdir ~/Documents  &&
-  rm ~/Documents/main.odt
+  sed -i '/]]/d' temp.md &&
+  pandoc temp.md --toc --citeproc --bibliography bibliography.bib --shift-heading-level-by=-1 \
+  --reference-doc ~/Templates/word_manuscript.docx -o ~/Documents/main.docx &&
+  rm temp.md
 }

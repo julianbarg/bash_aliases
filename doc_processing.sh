f570a770a330e818829f56d90934e97648e233f0
Julian Barg <barg.julian@gmail.com>
2021-12-20 09:13:20 -0500

    Updated doc_processing to work with the other format.

--- doc_processing.sh
+++ doc_processing.sh
@@ -1,11 +1,15 @@
 # For Factiva exports
 # Insert line seperator between articles
 
+# function factiva {
+#   filename=$(basename -- "$1")
+#   # extension="${filename##*.}"
+#   filename="${filename%.*}"
+#   less $1 > sed -z -i \
+#   "s/\nDocument/\n..............................................................................................\n\nDocument/g" \
+#   > "${filename}.txt"
+# }
+
 function factiva {
-  filename=$(basename -- "$1")
-  # extension="${filename##*.}"
-  filename="${filename%.*}"
-  less $1 > sed -z -i \
-  "s/\nDocument/\n..............................................................................................\n\nDocument/g" \
-  > "${filename}.txt"
+  sed -e 's=</div>=&</br>=g' -e 's=<span [^>]*>=&<h2>=g' -e 's=^</span>=&</h2>=g' $1 > $2
 }
